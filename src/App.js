aa5d3a623651148be9223824910da9c6001a0d62
anthonyvigz <anthonyvigz@gmail.com>
2019-06-06 19:43:03 -0400

    starting over, recreating App class using layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: "wash the floor",
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: "cook lobster",
+          id: Date.now() + 1,
+          completed: false
+        }
+      ],
+      todo: ""
+    };
+  }
+  
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList todoitems={this.state} todoToggle={this.todoToggle} />
+        <TodoForm
+          addHandler={this.addItem}
+          changeHandler={this.changeHandler}
+          value={this.state.todo}
+          clearCompleted={this.clearCompleted}
+        />
       </div>
     );
   }
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todo = props => {
+    return (
+        <h3>hey</h3>
+    )
+}
+
+
+
+export default Todo;
