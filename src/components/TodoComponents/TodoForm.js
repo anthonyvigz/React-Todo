7abc1d82a300252e27ca766e110a1a1f6a96deee
anthonyvigz <anthonyvigz@gmail.com>
2019-06-06 19:44:42 -0400

    created todoform component

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const TodoForm = props => {
-    
-}
+  return (
+    <form>
+      <input type="text" onChange={props.changeHandler} value={props.value} />
+      <button onClick={props.addHandler}>Add Todo</button>
+      <button onClick={props.clearCompleted}>Clear Completed</button>
+    </form>
+  );
+};
 
-
-export default TodoForm; 
+export default TodoForm;
